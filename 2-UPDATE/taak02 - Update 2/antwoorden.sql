fca8cf0027000a6645eb68ce110c564198a29dd8
cristian Jourdan <2106549@talnet.nl>
2020-11-03 10:52:36 +0100

    update taak 2

--- 2-UPDATE/taak02 - Update 2/antwoorden.sql
+++ 2-UPDATE/taak02 - Update 2/antwoorden.sql
@@ -1,8 +1,16 @@
 -- Opdracht 1
-
+UPDATE studenten2
+SET klas = '9x'
+WHERE klas = '9a';
 -- Opdracht 2 
-
+UPDATE studenten2
+SET klas = '9z'
+WHERE student_id >7;
 -- Opdracht 3
-
+UPDATE studenten2
+SET woonplaats = 'Amsterdam'
+WHERE student_id <=2 OR student_id >7;
 -- Opdracht 4
-
+UPDATE studenten2
+SET woonplaats = 'Uithoorn'
+WHERE klas = '9x' AND woonplaats= 'Amsterdam';
