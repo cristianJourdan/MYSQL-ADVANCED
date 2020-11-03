f59880c2273466578065109a360ef5f179f00c28
cristian Jourdan <2106549@talnet.nl>
2020-11-03 10:24:33 +0100

    insert taak 1

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,18 @@
 
+1. INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('23', 'cristian','jourdan', 'lctaa09c', 'aalsmeer', '1432','2009-10-7', 'Almere');
+
+2. INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+ VALUES ('24', 'peter','aap', 'lctaa09c', 'amstelveen', '1332','2006-12-13', 'Almere')
+
+ 3. INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('25', 'luc','siebeling', 'lctaa09c', 'skeerhoorn', '1211','2002-6-7', 'Almere');
+
+4.INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('26', 'niels','wendel', 'lctaa09c', 'aalsmeer', '1381','2001-2-5', 'Almere');
+
+5.INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('27', 'corne','van der meer', 'lctaa09c', 'amstelveen', '1321','20018-2-5', 'Almere');
+
+6.INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES ('28', 'jelle','hoogervorst', 'lctaa09c', 'aalsmeer', '1455','2003-10-28', 'Almere');
